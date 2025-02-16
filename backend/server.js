dfa15b82dc6197fbbd743e9338ecc16c966ed93e
aan147257 <aan147257@stud.gibb.ch>
2025-02-16 17:18:11 +0100

    made create and get books functional

--- backend/server.js
+++ backend/server.js
@@ -7,37 +7,38 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// Function to create a new MySQL connection
+
 const createDBConnection = async () => {
     return mysql.createConnection({
-        host: 'db', // Use 'db' as the hostname (Docker service name)
+        host: 'db',
         user: 'root',
         password: 'password',
         database: 'mugglemedia',
     });
 };
 
-// Function to wait for MySQL to be ready
+
 const waitForDB = async () => {
     let db;
     while (true) {
         try {
             db = await createDBConnection();
             console.log('MySQL connected...');
-            break; // Exit the loop if the connection is successful
+            break;
         } catch (err) {
             console.error('Waiting for MySQL...', err.message);
-            await new Promise(resolve => setTimeout(resolve, 5000)); // Retry after 5 seconds
+            await new Promise(resolve => setTimeout(resolve, 5000));
         }
     }
     return db;
 };
 
-// Function to start the Express server
-const startServer = async () => {
-    const db = await waitForDB(); // Wait for the database connection
 
-    // Routes for Books
+const startServer = async () => {
+    const db = await waitForDB();
+
+
+
     app.get('/api/books', async (req, res) => {
         try {
             const [results] = await db.query('SELECT * FROM books');
@@ -53,7 +54,7 @@ const startServer = async () => {
         const query = 'INSERT INTO books (title, author, description, release_date) VALUES (?, ?, ?, ?)';
         try {
             const [result] = await db.query(query, [title, author, description, release_date]);
-            res.send({ id: result.insertId, ...req.body }); // Return the inserted book data
+            res.send({ id: result.insertId, ...req.body });
         } catch (err) {
             console.error('Error adding book:', err.message);
             res.status(500).send('Error adding book');
@@ -66,5 +67,4 @@ const startServer = async () => {
     });
 };
 
-// Start the server
 startServer();
